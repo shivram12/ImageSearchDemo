719186c6ad09918c8ac48164cd0cbd650f3fac51
shivram12 <shivramshrestha01@gmail.com>
2020-10-30 23:31:29 +0545

    first commit

---
+++ app/src/main/java/com/shivram/imagesearchdemoapp/ui/gallery/fragment/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.shivram.imagesearchdemoapp.ui.gallery.fragment
+
+import androidx.fragment.app.Fragment
+import com.shivram.imagesearchdemoapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
